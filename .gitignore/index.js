7aca79f4f073747fa53173ad31d3e439271d62f8
Flexigaming <46349789+Flexigaming@users.noreply.github.com>
2019-01-03 15:54:56 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -18,5 +18,17 @@ bot.on('message', message => {
     if (message.content === prefix + "Salut"){
         message.reply("Bonjour tout le monde");
         console.log("commande Hey Vient d'être effectué");
-    } 
+    }
+
+    if (message.content === prefix + "embed"){
+        var embed = new Discord.RichEmbed()
+            .setTitle("EMBED")
+            .setDescription("Ceci est un Embed")
+            .addField(".help", "Page d'aide", true)
+            .addField("Embed01","chaine de [Flexigaming](https://www.youtube.com/channel/UCqYbjFVLK64KsStYNYZTlWA)", true)
+            .setColor("58FAF4")       
+            .setFooter("Abbonez vous :)")    
+        Message.channel.sendEmbed(embed); 
+
+    }
 });
